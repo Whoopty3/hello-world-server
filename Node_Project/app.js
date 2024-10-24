b8e7ab6432f5fac804855cdaca731bb66b139112
Payne Whitlock <paynewhitlock@Paynes-MBP-3.lan>
2024-10-23 23:56:56 -0400

    upload

--- Node_Project/app.js
+++ Node_Project/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 
 // Serve static files from the Node_Project/Assignments folder
 app.use(express.static(path.join(__dirname, 'Node_Project', 'Assignments')));
