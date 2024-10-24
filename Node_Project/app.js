8bc6d061a2b7b81601876c8822c5b81739677d2d
Payne Whitlock <paynewhitlock@Paynes-MBP-3.lan>
2024-10-23 23:22:22 -0400

    upload

--- Node_Project/app.js
+++ Node_Project/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 
+// Serve static files from the Node_Project/Assignments folder
+app.use(express.static(path.join(__dirname, 'Node_Project', 'Assignments')));
+
+// Route to serve the index.html file
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, 'Node_Project', 'Assignments', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
